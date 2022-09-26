be80882f49e177f853666637a254a49e7c1fa661
Sahana-LJ <113343371+Sahana-LJ@users.noreply.github.com>
2022-09-26 20:29:23 +0530

    BookMyShow
    Co-Authored-By: PrathibhaNM <91143506+PrathibhaNM@users.noreply.github.com>
    Co-Authored-By: RACHANA PUROHITH <55314352+Rachana14rmp@users.noreply.github.com>
    Co-Authored-By: priyankacs123 <104245585+priyankacs123@users.noreply.github.com>

---
+++ Template-for-Book-My-Show-Giftcard-main/README.md
@@ -0,0 +1,1 @@
+# Template-for-Book-My-Show-Giftcard
---
+++ Template-for-Book-My-Show-Giftcard-main/gfg/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_HOST_USER = '4vv19cs116@vvce.ac.in'
+EMAIL_HOST_PASSWORD = 'vvce@123'
+EMAIL_PORT = 587
